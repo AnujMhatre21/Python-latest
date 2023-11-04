1df21bb94a771fabfa5b3883d26cd8366141fd7d
AnujMhatre <anujmhatre21@gmail.com>
2023-11-04 15:33:25 +0530

    django demo completed

---
+++ django-simple/demo/myapp/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render, HttpResponse
+from .models import ToDoList
+
+# Create your views here.
+
+def home(request):
+    return render(request, "home.html")
+
+def todos(request):
+    items = ToDoList.objects.all()
+    return render(request, 'todolist.html',{'todos': items})
